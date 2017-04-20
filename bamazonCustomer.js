717950bc6009db638a86b5c8abccc7174099f084
Maxwell Pazevic <maxpazevic@Maxwells-MacBook-Pro.local>
2017-04-20 13:52:06 -0700

    Completed customer and manager sections

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 const formatCurrency = require('format-currency');
 require("console.table");
 var truthy = true;
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	database : 'Bamazon'
@@ -21,7 +21,7 @@ const fetchProducts = () => {
 		console.log("");
 		console.table(res);
 		initializeBamazon();
-	});	
+	});
 };
 
 //Only make the banner appear the first time the user runs the program
@@ -34,6 +34,7 @@ const displayBamazonBanner = () => {
 
 // Initial inquiry
 const initializeBamazon = () => {
+	//Display banner with first transaction
 	displayBamazonBanner();
 	let properties = {
 		properties: {
@@ -63,17 +64,36 @@ const initializeBamazon = () => {
 
 const checkInputs = (res) => {
 	//User cannot leave fields blank or enter a product ID that does not exist
-	if (res.id === '' || res.amount === '') {
-		console.log("\nOops! Looks like you left a field (or two) blank. Try again!\n");
-		initializeBamazon();
-	} else if (Number(res.id) > 10 || Number(res.id) < 1) {
-		console.log("\nThe ID that you entered doesn't exist! Try again you silly goose.\n");
+	if (res.id === '' && res.amount === '') {
+		//Give user the option to exit if two blanks are entered
+		console.log("");
+		giveOptionToExit();
+	} else if (res.id === '' || res.amount === '') {
+		//If only one blank, allow the user to try again
+		console.log("\nOops! Looks like you left a field blank. Try again!\n");
 		initializeBamazon();
 	} else {
-		checkBamazonInventory(res.id, res.amount);
+		//Otherwise, check to see that the selected item exists in the database
+		queryCheck(res.id, res.amount);
 	};
 };
 
+const queryCheck = (selectedId, queryAmount) => {
+	//Check if the selected item ("selectedId") exists by querying the database
+	connection.query("SELECT item_id, stock_quantity FROM products WHERE item_id=?", [selectedId], (err, res) => {
+		//If nothing is returned (i.e. the result array is 0 in length), tell the user that that item
+		//does not exist, and allow them to try again
+		if (res.length === 0) {
+			console.log("\nThe ID of the item you're looking for doesn't exist! Try again.\n");
+			initializeBamazon();
+			//Otherwise, check the inventory for their query
+		} else {
+			checkBamazonInventory(selectedId, queryAmount);
+		};
+	});
+};
+//Check database for user queried item and update database if there is a sufficient quantity
+//of the selected item available
 const checkBamazonInventory = (item_id, stock_quantity) => {
 	let userId = Number(item_id);
 	let userQuantity = Number(stock_quantity);
@@ -86,20 +106,21 @@ const checkBamazonInventory = (item_id, stock_quantity) => {
 			console.log("\nInsufficient quantity available (sorry)! Try again!\n");
 			initializeBamazon();
 		} else {
-			//Otherwise, update the database to match user input and print the total cost
+			//Otherwise, update the database to match user input
 			updateDatabaseQuantities(databaseStock, databasePrice, userQuantity, userId);
 		};
 	});
 };
 
 const updateDatabaseQuantities = (databaseStock, databasePrice, userQuantity, userId) => {
+	//Set database quantities to reflect user query
 	connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?", [(databaseStock - userQuantity), userId], (err, res) => {
 		console.log("\nYour order went through!\n");
 		// Ensure that the cost statement follows the order confirmation statement by
 		// including it in the updateDatabaseQuantities function
 		calculateTotalCost(databasePrice, userQuantity);
 		//Ask the user if he/she wishes to continue shopping
-		runSecondInquiry();
+		runContinueInquiry();
 	});
 };
 
@@ -108,7 +129,24 @@ const calculateTotalCost = (databasePrice, userQuantity) => {
 	console.log("That hypothetical transaction cost you: " + "$" + formatCurrency(Number(databasePrice) * Number(userQuantity)) + "\n");
 };
 
-const runSecondInquiry = () => {
+const giveOptionToExit = () => {
+	inquirer.prompt([{
+		name: "check",
+		type: "list",
+		message: "Hmmm..do you want to return to the selection menu or exit the program?",
+		choices: ["Return to the selection menu", "Exit the program"],
+		default: 0
+	}]).then((answers) => {
+		if (answers.check === "Return to the selection menu") {
+			fetchProducts();
+		} else if (answers.check === "Exit the program") {
+			connection.end();
+		};
+	});
+};
+
+//Inquiry for asking whether or not user should continue
+const runContinueInquiry = () => {
 	inquirer.prompt([{
 		type: 'list',
 		name: 'shopping',
@@ -127,4 +165,3 @@ const runSecondInquiry = () => {
 
 //Function called to print the table and begin the inquiry
 fetchProducts();
-// connection.end();
